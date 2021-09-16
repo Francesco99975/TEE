bf3cabe8e348dbdbecfa5aff8a602337f92db37a
Francesco Michele Barranca <francescomich99@gmail.com>
2021-09-16 12:28:22 -0400

    added keyprotection logic

--- src/controllers/tee.ts
+++ src/controllers/tee.ts
@@ -1,14 +1,22 @@
 import { Request, Response, NextFunction } from "express";
-import { encrypt, decrypt, checkData } from "../helpers/encryption";
+import { encrypt, decrypt, checkData, checkKey } from "../helpers/encryption";
 
 export async function onEncrypt(req: Request, res: Response, next: NextFunction) {
     try {
-        await checkData(req.body);
+        await checkData(req.body.data);
 
         let response = {};
 
-        for (const key in req.body) {
-            response = {...response, [key]: await encrypt(req.body[key])};
+        if(req.body.key && req.body.key.length > 0) {
+            await checkKey(req.body.key);
+
+            for (const key in req.body.data) {
+                response = {...response, [key]: await encrypt(req.body.data[key], req.body.key)};
+            }
+        } else {
+            for (const key in req.body.data) {
+                response = {...response, [key]: await encrypt(req.body.data[key])};
+            }
         }
 
         return res.json(response);
@@ -21,9 +29,15 @@ export async function onDecrypt(req: Request, res: Response, next: NextFunction)
     try {
         let response = {};
 
-        for (const key in req.body) {
-            response = {...response, [key]: await decrypt(req.body[key])};
-        }
+        if(req.body.key && req.body.key.length > 0) {
+            for (const key in req.body.data) {
+                response = {...response, [key]: await decrypt(req.body.data[key], req.body.key)};
+            }
+        } else {
+            for (const key in req.body.data) {
+                response = {...response, [key]: await decrypt(req.body.data[key])};
+            }
+        }    
 
         return res.json(response);
     } catch (error) {
--- src/helpers/encryption.ts
+++ src/helpers/encryption.ts
@@ -1,6 +1,7 @@
 import crypto from "crypto";
 import Encode from "../models/encode";
 import BigNumber from "bignumber.js";
+import { HttpException } from "../interfaces/error";
 
 const BigBase64 = {
 
@@ -86,8 +87,33 @@ export async function checkData(data: any) {
     }
 }
 
-export async function encrypt(message: string): Promise<string> {
+export async function checkKey(key: string) {
+    for(let x of key) {
+        let encodes = await Encode.find({character: x}) as [];
+        if(encodes.length == 0) {
+            await generateRandomEncode(x)
+        }
+    }
+}
+
+export async function encrypt(message: string, key?: string): Promise<string> {
     let encryptedMessage: string = "";
+
+    if(key) {
+        encryptedMessage += process.env.KEY_ON!;
+        encryptedMessage += key.length.toString(2).padStart(+process.env.KEY_PAD!, '0');
+
+        for(let x of key) {
+            let randomizer = generateBinaryString(+process.env.ENCRYPTION_MASK_MIN!, +process.env.ENCRYPTION_MASK_MAX!);
+    
+            const encode = await Encode.find({character: x});
+            let encodedChar = encode[0].code; // Get DB
+    
+            encryptedMessage += randomizer.length.toString(2) + zerOne() + encodedChar.length.toString(2).padStart(+process.env.PAD!, '0') + zerOne() + randomizer + encodedChar + zerOne();
+        }
+    } else {
+        encryptedMessage += process.env.KEY_OFF!;
+    }
     
     for(let x of message) {
         let randomizer = generateBinaryString(+process.env.ENCRYPTION_MASK_MIN!, +process.env.ENCRYPTION_MASK_MAX!);
@@ -101,12 +127,41 @@ export async function encrypt(message: string): Promise<string> {
     return BigBase64.fromNumber(new BigNumber(encryptedMessage, 2));
 } 
 
-export async function decrypt(encryptedMessage64: string): Promise<string> {
+export async function decrypt(encryptedMessage64: string, key?: string): Promise<string> {
     let encryptedMessage = BigBase64.toNumber(encryptedMessage64).toString(2);
 
     let encryptedChar = '';
 
     let decryptedMessage = "";
+
+    const keyStatus = encryptedMessage.substring(0, +process.env.KEY_STS_LEN!);
+    encryptedMessage = encryptedMessage.substring(+process.env.KEY_STS_LEN!);
+
+    if(keyStatus === process.env.KEY_ON!) {
+        const keyLen = parseInt(encryptedMessage.substring(0, +process.env.KEY_PAD!), 2);
+        encryptedMessage = encryptedMessage.substring(+process.env.KEY_PAD!);
+
+        let decryptedKey = '';
+
+        for(let i = 0; i < keyLen; i++) {
+            let randomizer = parseInt(encryptedMessage.substring(0, +process.env.RAND_END!), 2);
+            let charLength = parseInt(encryptedMessage.substring(+process.env.LEN_START!, +process.env.LEN_END!), 2);
+    
+            let beginning: number = +process.env.ENCODE_START!;
+            beginning += randomizer;
+            let end = beginning + charLength;
+            encryptedChar = encryptedMessage.substring(beginning, end);
+    
+            const encode = await Encode.find({code: encryptedChar});
+            decryptedKey += encode[0].character; // Get from DB
+    
+            encryptedMessage = encryptedMessage.substring(beginning + charLength + 1);
+        }
+
+        if(key !== decryptedKey) throw new HttpException(401, "Wrong Passkey");
+    }
+
+
 
     while(encryptedMessage.length > 0) {
         let randomizer = parseInt(encryptedMessage.substring(0, +process.env.RAND_END!), 2);
